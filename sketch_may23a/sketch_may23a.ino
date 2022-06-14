b70b67c9592b22f6f17c03a1d3ec067d6bf855c5
gibsonow <70610481+gibsonow@users.noreply.github.com>
2022-06-14 00:22:08 -0400

    Updated .ino
    Reconfigured buttons to "1234" input, added Doxygen-style comment blocks

--- sketch_may23a/sketch_may23a.ino
+++ sketch_may23a/sketch_may23a.ino
@@ -1,24 +1,14 @@
-/**
- * @file sketch_may23a.ino
- * @author Owen Gibson (gibsonow@mail.gvsu.edu)
- * @brief Program for embedded arduino system used in CIS350 Final Project
- * @version 0.1
- * @date 2022-06-01
- * 
- * @copyright Copyright (c) 2022
- * 
+/*
+ * Author:  Owen Gibson
+ * Project: CIS350 Embedded System Contribution
+ * Date:    5/23/21
  */
-
 #include <Keyboard.h>
 
 int backPin = 9;                  // Set button pins
 int pausePin = 10;
 int forwardPin = 4;
 
-/**
- * @brief Initialization loop (runs 1 time)
- * 
- */
 void setup()
 {
   pinMode(backPin, INPUT);        // Set the button as an input
@@ -33,10 +23,6 @@ void setup()
   Keyboard.begin();               //Init keyboard emulation
 }
 
-/**
- * @brief Continuous loop (runs until shutdown of device)
- * 
- */
 void loop()
 {
   if (digitalRead(backPin) == 0)  // if the button goes low
@@ -47,7 +33,7 @@ void loop()
     Keyboard.release(KEY_LEFT_CTRL);
     Keyboard.release(KEY_LEFT_ARROW);
 
-    delay(300);                   // debounce window
+    delay(300);
   }
   if (digitalRead(pausePin) == 0)  
   {
